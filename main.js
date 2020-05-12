a38f57d2dd5a444f1c4433293ca893ceae32ae85
alexei <webdesfreelance@gmail.com>
2020-05-12 16:07:16 +0300

    work save

---
+++ main.js
@@ -0,0 +1,9 @@
+//
+document.title="Today we manipulate with DOM";
+//
+/*let questionFrontEnd=confirm('Are You Front Developer?');
+if(questionFrontEnd==true)alert('Answer:Yes')
+else if(questionFrontEnd==false) alert('Answer:No')*/
+//
+
+   
